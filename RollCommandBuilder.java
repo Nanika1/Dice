0ec9988d5bf7eef2e161cdcda36bf041e77e4d43
Nanika1 <lilledragen92@gmail.com>
2026-06-21 17:10:32 +0200

    Updated RollCommandBuilder.java
    Added three methods for adding commandlettes, one for each roll type.

--- RollCommandBuilder.java
+++ RollCommandBuilder.java
@@ -9,6 +9,22 @@ public class RollCommandBuilder {
         _command.addCommandlette(dice, rollType, modifiers);
         return this;
     }
+
+    public RollCommandBuilder addDiceRoll(String dice, String... modifiers) {
+        _command.addCommandlette(dice, RollType.REGULAR, modifiers);
+        return this;
+    }
+
+    public RollCommandBuilder addDiceRollAdvantage(String dice, String... modifiers) {
+        _command.addCommandlette(dice, RollType.ADVANTAGE, modifiers);
+        return this;
+    }
+
+    public RollCommandBuilder addDiceRollDisadvantage(String dice, String... modifiers) {
+        _command.addCommandlette(dice, RollType.DISADVANTAGE, modifiers);
+        return this;
+    }
+
     public RollCommandBuilder addConstant(String value) {
         _command.addConstant(value);
         return this;
